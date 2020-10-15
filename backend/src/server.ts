c155b661f6d447c65fd3040280250821fc4835dc
fernandomarca <fernandomarca@hotmail.com>
2020-10-15 20:55:29 -0300

    config orm - routes orphanages

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-    return response.json({ message: 'hello world' })
-});
+app.use(routes);
 
 app.listen(3333);
