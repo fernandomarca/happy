4180b936455455fa91af4b446f336d158b73c669
fernandomarca <fernandomarca@hotmail.com>
2020-10-17 16:00:37 -0300

    create pages CreateOrphanage and Orphanage

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,49 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
-
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
+
 import '../styles/pages/orphanages-map.css';
 
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+})
+
 function OrphanagesMap() {
-    return (
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={mapMarkerImg} alt="mapMarker" />
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="mapMarker" />
 
-                    <h2>Escolha um orfanato no mapa</h2>
-                    <p>Muitas crianças estão esperando a sua visita :)</p>
-                </header>
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
 
-                <footer>
-                    <strong>Cascavel</strong>
-                    <span>Paraná</span>
-                </footer>
-            </aside>
+        <footer>
+          <strong>Cascavel</strong>
+          <span>Paraná</span>
+        </footer>
+      </aside>
 
-            <Map
-                center={[-24.935265, -53.4662277]}
-                zoom={15}
-                style={{ width: '100%', height: '100%' }}
-            >
-                {/**
+      <Map
+        center={[-24.935265, -53.4662277]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/**
                  <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                 */}
 
-                <TileLayer
-                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
-            </Map>
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
 
-            <Link to="" className="create-orphanage">
-                <FiPlus size={32} color="#fff" />
+        <Marker
+          icon={mapIcon}
+          position={[-24.935265, -53.4662277]}
+        >
+          <Popup
+            className="map-popap"
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+          >
+            Lar das meninas
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="#fff" />
             </Link>
-        </div>
-    );
+          </Popup>
+        </Marker>
+
+      </Map>
+
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
