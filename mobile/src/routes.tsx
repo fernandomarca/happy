11439f202628fe49a5901d40793821fcf590cd79
fernandomarca <fernandomarca@hotmail.com>
2020-10-18 19:31:11 -0300

    finished mobile

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -5,18 +5,47 @@ import { createStackNavigator } from '@react-navigation/stack';
 //components
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanagesDetails from './pages/OrphanagesDetails';
+import SelectedMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
 
 const { Navigator, Screen } = createStackNavigator();
 
 export default function Routes() {
-    return (
-        <NavigationContainer>
-            <Navigator screenOptions={{
-                headerShown: false
-            }}>
-                <Screen name="OrphanagesMap" component={OrphanagesMap} />
-                <Screen name="OrphanageDetails" component={OrphanagesDetails} />
-            </Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: '#f2f3f5'
+        }
+      }}>
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+
+        <Screen
+          name="OrphanageDetails"
+          component={OrphanagesDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orfanato" />
+          }}
+        />
+
+        <Screen name="SelectedMapPosition" component={SelectedMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa" />
+          }}
+        />
+
+        <Screen name="OrphanageData" component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados" />
+          }}
+        />
+
+      </Navigator>
+    </NavigationContainer>
+  )
 }
